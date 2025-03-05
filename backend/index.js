517416ee7b4e9e028882e4f2a9085c54ea3a7fd3
zubair-shabir <zubairshabir980@gmail.com>
2025-03-05 14:45:03 +0530

    Cors Error Fix

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,12 @@ app.get("/", (req, res) => {
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Allow all origins
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 app.use("/tasks", TaskRouter);
 
